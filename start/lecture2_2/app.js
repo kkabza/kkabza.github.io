d5fcb86b9300ae4e60870839dc60bf88c1871e1b
Keith Kabza <kkabza@microsoft.com>
2022-06-23 16:30:32 -0400

    Initial commit

---
+++ start/lecture2_2/app.js
@@ -0,0 +1,32 @@
+import * as THREE from '../../libs/three/three.module.js';
+import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+    
+        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
+		this.camera.position.set(0,0,4);
+
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE/THREE.Color(0xaaaaaa);
+
+		this.render = new THREE.WebGL1Renderer({antialias: true});
+		this.render.setPixelRation( window.devicePixelRatio);
+		this.render.setSize( window.innerWidth,window.innerHeight);
+		container.appendChild( this.render.bind(this.render.domElement));
+		this.render.setAnimationLoop(this.render.bind(this))
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    resize(){
+        
+    }
+    
+	render( ) {  
+        this.render.render(this.scene, this.camera);
+    }
+}
+
+export { App };
